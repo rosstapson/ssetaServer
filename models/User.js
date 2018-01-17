db513029cdfce7b2a4d1bd3d63b835bf25483e86
rosstapson <ross.tapson@gmail.com>
2018-01-17 10:42:53 +0200

    added some user details

--- models/User.js
+++ models/User.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 module.exports = new mongoose.Schema({
     username: String,
     email: String,
-    password: Date,
+    password: String,
+    company: String,
+    telephone: String,
+    division: String,
+    role: String,
+    address: String,
+    state: String,
+    country: String,    
+    created_at: Date,    
     id: mongoose.Schema.ObjectId
 })
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,39 @@
 // ./routes/users.js
 var mongoose = require('mongoose');
 var cors = require('cors');
-
+var jwt = require('jsonwebtoken');
 const User = require('../models/User');
+var config = require("../config");
 
 module.exports = (app) => {
   app.get('/users', (req, res) => {
       req.userModel.find({}).sort({'email': -1}).exec((err, users) => res.json(users))
   });
-//   app.get('/login', (req, res) => {
-
-//       res.send("zomg")
-//   });
+function createToken(user) {
+    if (!user.role) {
+        user.role = 'guest';
+    }
+    return jwt.sign({
+        username: user.email,
+        role: user.role
+    }, config.secret, {
+        expiresIn: 60 * 60 * 24
+    });
+}
   app.options('/login', cors());
   app.post('/login', (req, res) => {
       //res.json(req.body);
     var password = req.body.user.password;
     try {
         var query = req.userModel.findOne({'email': req.body.user.email});
-        query.select('email username password');
+        query.select('id email username password');
         query.exec((err, user) => {            
             if(err) {
                 res.status(400).send(err);
             }
             if (user && password === user.password) {
-                res.json(res.status(200).send("Success"));
+                user.token
+                res.json(res.status(200).send({token: createToken(user)}));
             }
             else {
                 res.status(400).send("No luck");
@@ -37,7 +46,7 @@ module.exports = (app) => {
   });
 
   app.post('/users', (req, res) => {
-      const newuser = new req.userModel(Object.assign({}, req.body, {created_at: Date.now()}));
+      const newuser = new req.userModel(Object.assign({}, req.body.user, {created_at: Date.now()}));
       newuser.save((err, saveduser) => {
           res.json(saveduser)
       })
