f72e11bfd26d7b289bcaada929c94d240bcd4c44
rosstapson <ross.tapson@gmail.com>
2018-01-18 18:47:57 +0200

    refactor for ignite

--- middleware/db.js
+++ middleware/db.js
@@ -4,7 +4,7 @@ const QuestionnaireSchema = require('../models/Questionnaire');
 
 module.exports = {    
     connectDisconnect: (req, res, next) => {        
-        const connection = mongoose.createConnection(req.webtaskContext.secrets.MONGO_URL);        
+        const connection = mongoose.connect('mongodb://localhost/sseta');
         req.userModel = connection.model('User', UserSchema);
         req.questionnaireModel = connection.model('Questionnaire', QuestionnaireSchema);
         req.on('end', () => {           
