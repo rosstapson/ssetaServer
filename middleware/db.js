93847bafa2b157a755b24302d507a2c4e19a9047
rosstapson <ross.tapson@gmail.com>
2018-01-19 14:15:04 +0200

    more db, and some config

--- middleware/db.js
+++ middleware/db.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 const UserSchema = require('../models/User');
 const QuestionnaireSchema = require('../models/Questionnaire');
+var config = require("../config");
 
 module.exports = {    
-    connectDisconnect: (req, res, next) => {        
-        const connection = mongoose.connect('mongodb://sseta:yoda100@localhost:27017/sseta');
+    connectDisconnect: (req, res, next) => {
+        const connection = mongoose.connect(config.DB_URL);
         req.userModel = connection.model('User', UserSchema);
         req.questionnaireModel = connection.model('Questionnaire', QuestionnaireSchema);
         req.on('end', () => {           
