8e16c74fcbac0cacce7c366060625192bef5f3e3
rosstapson <ross.tapson@gmail.com>
2018-01-19 14:24:08 +0200

    db debug

--- middleware/db.js
+++ middleware/db.js
@@ -7,16 +7,16 @@ module.exports = {
     connectDisconnect: (req, res, next) => {
         try {
             const connection = mongoose.connect(config.DB_URL);        
-            connection.on('error', console.error.bind(console, 'connection error:'));
+            connection.on('error', console.error.bind(console, 'connection error:'));        
+            req.userModel = connection.model('User', UserSchema);
+            req.questionnaireModel = connection.model('Questionnaire', QuestionnaireSchema);
+            req.on('end', () => {           
+                mongoose.connection.close();
+            })        
+            next()
         }
         catch(err) {
-            console.log(err)
+            console.log(err);
         }
-        req.userModel = connection.model('User', UserSchema);
-        req.questionnaireModel = connection.model('Questionnaire', QuestionnaireSchema);
-        req.on('end', () => {           
-            mongoose.connection.close();
-        })        
-        next()
     },
 }
