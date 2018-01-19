bf6da7a929fea8707070dcf806c643d37e0dde4c
rosstapson <ross.tapson@gmail.com>
2018-01-19 15:13:37 +0200

    breaking changes, it seems

--- middleware/db.js
+++ middleware/db.js
@@ -6,7 +6,7 @@ var config = require("../config");
 module.exports = {    
     connectDisconnect: (req, res, next) => {
         try {
-            const connection = mongoose.connect(config.DB_URL);              
+            const connection = mongoose.createConnection(config.DB_URL);              
             req.userModel = connection.model('User', UserSchema);
             req.questionnaireModel = connection.model('Questionnaire', QuestionnaireSchema);
             req.on('end', () => {           
