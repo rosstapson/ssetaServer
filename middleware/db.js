7dd0bd97a9d3b576808c564dcff1403703f96fff
rosstapson <ross.tapson@gmail.com>
2018-01-19 14:12:06 +0200

    db

--- middleware/db.js
+++ middleware/db.js
@@ -4,7 +4,7 @@ const QuestionnaireSchema = require('../models/Questionnaire');
 
 module.exports = {    
     connectDisconnect: (req, res, next) => {        
-        const connection = mongoose.connect('mongodb://localhost/sseta');
+        const connection = mongoose.connect('mongodb://sseta:yoda100@localhost:27017/sseta');
         req.userModel = connection.model('User', UserSchema);
         req.questionnaireModel = connection.model('Questionnaire', QuestionnaireSchema);
         req.on('end', () => {           
