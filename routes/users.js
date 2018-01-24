da44fd5aa1cf289d48d87fce9f231e039c4b3a16
Cloud User <centos@mcpy5.isignite.co.za.novalocal>
2018-01-24 07:17:03 +0000

    on point...

--- routes/users.js
+++ routes/users.js
@@ -32,16 +32,21 @@ function createToken(user) {
   app.options('/login', cors());
   app.post('/login', (req, res) => {
     var c = new Client(config.DB_CONFIG);
-    
-    c.query('SELECT * FROM users WHERE email= :email', {email: req.body.email},  function(err, rows) {
+//console.log('email on request body: ' + req.body.user.email);    
+    c.query('SELECT * FROM users WHERE email= :email', {email: req.body.user.email},  function(err, rows) {
       if (err) {
         console.log(err);
         res.status(400).send(err);
       }
       else {
          // `rows.info.metadata` contains the metadata
-      console.dir(rows);        
+      console.dir(rows.info.numRows);
+      if (rows.info.numRows === '0') {
+        res.status(400).send("Invalid User Credentials"); 
       }
+      else {
+        res.status(200).send("Huzzah");
+      }}
     });
     
     c.end();
