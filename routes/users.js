97bc98d09e9f2495411462a78ab86d3d9bde57d8
Cloud User <centos@mcpy5.isignite.co.za.novalocal>
2018-01-24 08:52:32 +0000

    registration working.

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var Client = require('mariasql');
 var cors = require('cors');
 var jwt = require('jsonwebtoken');
 var config = require("../config");
+var cuid = require("cuid");
 
 module.exports = (app) => {
   app.get('/users', (req, res) => {
@@ -44,9 +45,16 @@ function createToken(user) {
         if (rows.info.numRows === '0') {
           res.status(400).send("Invalid User Credentials"); 
         }
-        else {
-          var token = createToken(req.body.user);
-          res.status(200).send({token: token});
+        else 
+        {
+          
+          if (req.body.user.password !== rows[0].password) {
+             res.status(400).send("Invalid User Credentials");
+          }
+          else {
+             var token = createToken(req.body.user);
+             res.status(200).send({token: token});
+          }
         }
       }
     });
@@ -54,10 +62,39 @@ function createToken(user) {
     c.end();
   });
 
-  app.post('/users', (req, res) => {
-      console.log("post /users");
-      
-  })
+  app.post('/users', (req, res) =>   {
+    var user = req.body.user;
+    var id = cuid();
+    if (!user.email || !user.password) {
+      res.status(400).send("Invalid user credentials");
+    }
+    else {
+      var c = new Client(config.DB_CONFIG);
+
+    c.query('INSERT INTO users (name, email, password, company, division, role, access_level, telephone, address, state, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+     [ 
+        user.name,
+        user.email,
+        user.password,
+        user.company,
+        user.division,
+        user.role,
+        user.accessLevel,
+        user.telephone,
+        user.address,
+        user.state,
+        user.country
+      ],  function(err, rows) {
+      if (err) {
+        console.log(err);
+        res.status(400).send(err);
+      }
+      else {
+        res.status(201).send("Success");
+      }
+    })
+  }
+});
 
   app.put('/users', (req, res) => {
     console.log("put /users")
