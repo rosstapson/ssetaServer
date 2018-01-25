3e76a6f1c9a877b9423de40f63593d5e43ff50a6
rosstapson <ross.tapson@gmail.com>
2018-01-25 15:21:43 +0200

    add questionnaire...

--- routes/questionnaires.js
+++ routes/questionnaires.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
         console.log("get /questionnaire_list");
     });
     app.post('/questionnaire_list', (req, res) => {
-        console.log("token: " + JSON.stringify(req.body.email));
+        
         var decoded = jwt.verify(req.body.token, config.secret);
         if (!decoded) {
             res.status(400).send("Invalid token");
@@ -24,8 +24,29 @@ module.exports = (app) => {
     });
     app.post('/questionnaires', (req, res) => {
         var decoded = jwt.verify(req.body.token, config.secret);
+        if (!decoded) {
+            res.status(400).send("Invalid token");
+        }
         var questionnaire = req.body.questionnaire;
         questionnaire.createdBy = 'bob';
-        
+        var c = new Client(config.DB_CONFIG);
+        c.query('INSERT INTO questionnaires (name, reference, training_provider, client_company, client_division) VALUES (?, ?, ?, ?, ?)',
+      [ 
+        questionnaire.name,
+        questionnaire.reference,
+        questionnaire.trainingProvider,
+        questionnaire.clientCompany,
+        questionnaire.clientDivistion          
+        ],  function(err, rows) {
+        if (err) {
+          console.log(err);
+          res.status(400).send(err);
+        }
+        else { 
+            console.log(rows);
+            res.status(201).send("Questionnaire Saved");
+        }
+        });
+
     });
 }
